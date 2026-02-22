a2b3be6fb73c5b62dfccc4191b6224b5628e2782
saiful3278G <mdm439382@gmail.com>
2026-02-22 23:04:15 +0800

    v

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,11 +25,7 @@ android {
                 storeFile = file(storeFilePath)
             }
             val storeTypeEnv = System.getenv("KEYSTORE_TYPE")
-            if (!storeTypeEnv.isNullOrBlank()) {
-                storeType = storeTypeEnv
-            } else {
-                storeType = "pkcs12"
-            }
+            storeType = if (!storeTypeEnv.isNullOrBlank()) storeTypeEnv else "jks"
             storePassword = System.getenv("STORE_PASSWORD")
             keyAlias = System.getenv("KEY_ALIAS")
             keyPassword = System.getenv("KEY_PASSWORD")
